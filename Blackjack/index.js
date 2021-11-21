94fd20c849de56e6c86cb829f35b10b9cea73199
lucas mira <lucas-mira1@hotmail.com>
2021-11-21 15:15:40 -0300

    21/11/2021

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -1,16 +1,43 @@
-let firstCard = getRandomInt(2,12)
-let secondCard = getRandomInt(2,12)
-
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
 
-console.log('you first card is: ' + firstCard)
-console.log('your second card is: ' + secondCard)
-console.log('final points is: ' + (firstCard + secondCard))
+let sum
 
-if (firstCard + secondCard < 21) console.log('do you want to draw a new card?')
-else if (firstCard + secondCard > 21) console.log('sorry, you lose.')
-else console.log('congratulations! you won')
+let startGame = () => {
+  let firstCard = getRandomInt(2,12)
+  let secondCard = getRandomInt(2,12)
+  sum = firstCard + secondCard
+  let hasBlackJack = false
+  let isAlive = true
+  let message = ""
+  document.getElementById("cards-el").textContent = 'Cards: ' + firstCard + ' + ' + secondCard
+  result()
+}
+
+let result = ()=> {
+  document.getElementById("sum-el").textContent = "sum: " + sum
+  if (sum <= 20) message = 'do you want to draw a new card?'
+  else if (sum === 21) {
+    message = 'congratulations! you won.'
+    hasBlackJack = true
+  }
+  else {
+    message = 'sorry, you lose.'
+    isAlive=false
+  }
+
+  document.getElementById("message-el").textContent = message
+}
+
+let newCard = ()=>{
+  if (!sum) document.getElementById("message-el").textContent = "begin a game first"
+  else {
+    let nextCard = getRandomInt(2,12)
+    document.getElementById("cards-el").textContent += " + " + nextCard
+    sum += nextCard
+    result()
+  }
+}
