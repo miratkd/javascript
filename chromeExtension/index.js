97278c729952977e39ba3ce722be428c31223abd
lucas mira <lucas-mira1@hotmail.com>
2021-12-01 21:20:11 -0300

    01/12 5:36

--- chromeExtension/index.js
+++ chromeExtension/index.js
@@ -1,4 +1,15 @@
 let myLeads = []
 let inputEl = document.getElementById('input-el')
+let ulEl = document.getElementById('ul-el')
 
-let save = ()=> console.log('')
+let save = ()=> {
+  myLeads.push(inputEl.value)
+  inputEl.value = ''
+  renderList()
+}
+
+let renderList = () => {
+  let list = ''
+  myLeads.forEach(lead => list += '<li><a href="'+lead+'" target="_blank">'+lead+'</a></li>')
+  ulEl.innerHTML = list
+}
