79765518733d8b3e8cdd543b4e6f898ba391ce29
lucas mira <lucas-mira1@hotmail.com>
2021-12-04 21:04:00 -0300

    04/12 6:44

--- chromeExtension/index.js
+++ chromeExtension/index.js
@@ -1,7 +1,10 @@
-let myLeads = JSON.parse(localStorage.getItem('myLeads'))
+let myLeads
+if (JSON.parse(localStorage.getItem('myLeads'))) myLeads = JSON.parse(localStorage.getItem('myLeads'))
+else myLeads = []
 let inputEl = document.getElementById('input-el')
 let ulEl = document.getElementById('ul-el')
 let inputBtn = document.getElementById('input-btn')
+let deleteBtn = document.getElementById('delete-btn')
 
 let renderList = () => {
   let list = ''
@@ -9,10 +12,17 @@ let renderList = () => {
   ulEl.innerHTML = list
 }
 let save = ()=> {
+  console.log(myLeads)
   myLeads.push(inputEl.value)
   inputEl.value = ''
   localStorage.setItem("myLeads", JSON.stringify(myLeads))
   renderList()
 }
+let deleteAll = () => {
+  localStorage.removeItem('myLeads')
+  myLeads = []
+  renderList()
+}
 renderList()
 inputBtn.addEventListener('click', save);
+deleteBtn.addEventListener('dblclick', deleteAll)
